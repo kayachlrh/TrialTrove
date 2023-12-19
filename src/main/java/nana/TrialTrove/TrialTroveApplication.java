98b84824053924fb52af5be02f926455d59ddd6b
nana <o3oboo@daum.net>
2023-12-19 16:16:51 +0900

    first commit

---
+++ src/main/java/nana/TrialTrove/TrialTroveApplication.java
@@ -0,0 +1,13 @@
+package nana.TrialTrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialTroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialTroveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nana/TrialTrove/TrialTroveApplicationTests.java
@@ -0,0 +1,13 @@
+package nana.TrialTrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialTroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
