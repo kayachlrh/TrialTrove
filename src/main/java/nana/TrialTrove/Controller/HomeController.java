4897332fe40471233eb307ca7bb0bf62b1f31fb7
nana <o3oboo@daum.net>
2023-12-29 23:15:51 +0900

    front main

---
+++ src/main/java/nana/TrialTrove/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package nana.TrialTrove.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String main(){
+
+        return "index";
+    }
+}
